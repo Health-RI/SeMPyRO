3d7939f2ad48b8774d853462fb0b75e8cf3ddc45
anastasianayden <anastasia.nayden@health-ri.nl>
2024-02-05 14:24:18 +0100

    ADMS and DCATv3 namespaces

--- dcat/DCATv3.py
+++ dcat/DCATv3.py
@@ -2,5 +2,11 @@ from rdflib import DCAT, URIRef
 
 
 class DCATv3(DCAT):
-    """Extends DCAT namespace with parameters added in v3"""
+    """Extends DCAT namespace with properties added in v3"""
     version: URIRef
+    previousVersion: URIRef
+    hasCurrentVersion: URIRef
+    first: URIRef
+    last: URIRef
+    prev: URIRef
+    inSeries: URIRef
