fa540276922723dd3dc80368eafab2923965668a
anastasianayden <anastasia.nayden@health-ri.nl>
2024-04-08 10:31:27 +0200

    fix: fix timestamp serialization bug

--- __tests__/test_rdf_model.py
+++ __tests__/test_rdf_model.py
@@ -15,16 +15,18 @@
 from sempyro import RDFModel
 from datetime import date, datetime
 from pydantic import AwareDatetime, NaiveDatetime, field_validator
-from rdflib import Literal, XSD
+from rdflib import Literal, XSD, URIRef, Graph
+from rdflib.compare import to_isomorphic
 import pytest
 from typing import Union
 from sempyro.utils.validator_functions import date_handler
+from sempyro.dcat import DCATDataset
 
 
 @pytest.mark.parametrize("date_input,output", [
     ("1992", Literal("1992", datatype=XSD.gYear),),
-    (1707995797, Literal("2024-02-15 11:16:37+00:00", datatype=XSD.dateTime)),
-    ("November 9, 1999", Literal("1999-11-09 00:00:00", datatype=XSD.dateTime)),
+    (1707995797, Literal("2024-02-15T11:16:37+00:00", datatype=XSD.dateTime)),
+    ("November 9, 1999", Literal("1999-11-09T00:00:00", datatype=XSD.dateTime)),
     (datetime.date(datetime.now()), Literal(str(datetime.now().date()), datatype=XSD.date)),
     ("2006-09", Literal("2006-09", datatype=XSD.gYearMonth))
 ])
@@ -40,3 +42,31 @@ def test_time_literal(date_input, output):
     obj = myModel(date_field=date_input)
     expected = obj._convert_to_datetime_literal(obj.date_field)
     assert expected == output
+
+
+@pytest.mark.parametrize("issued,expected_date",
+                         [
+                             ("1992", "\"1992\"^^xsd:gYear"),
+                             ("November 9, 1999", "\"1999-11-09T00:00:00\"^^xsd:dateTime"),
+                             ("2006-09", "\"2006-09\"^^xsd:gYearMonth"),
+                             ("25-05-1998", "\"1998-05-25T00:00:00\"^^xsd:dateTime"),
+                             (datetime.now().date(), f"\"{(str(datetime.now().date()))}\"^^xsd:date")
+                         ]
+                         )
+def test_time_serialization(issued, expected_date):
+    title = ["Test title"]
+    description = ["Test description"]
+    expected = (f"@prefix dcat: <http://www.w3.org/ns/dcat#> .\n"
+                f"@prefix dcterms: <http://purl.org/dc/terms/> .\n"
+                f"@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n"
+                f"<http://example.com/1> a dcat:Dataset ;\n"
+                f"dcterms:description \"Test description\" ;\n"
+                f"dcterms:issued {expected_date} ;\n"
+                f"dcterms:title \"Test title\" .\n")
+    dataset = DCATDataset(title=title,
+                          description=description,
+                          release_date=issued
+                          )
+    actual = dataset.to_graph(URIRef("http://example.com/1"))
+    expected = Graph().parse(data=expected, format="ttl")
+    assert to_isomorphic(actual) == to_isomorphic(expected)
