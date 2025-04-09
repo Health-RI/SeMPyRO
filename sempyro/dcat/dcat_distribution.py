31065395c42e355afba0709e98e08af28b10fb53
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2025-04-09 15:54:24 +0200

    feat(dataservice) Add dataservice to the GDI use case

--- sempyro/dcat/dcat_distribution.py
+++ sempyro/dcat/dcat_distribution.py
@@ -112,7 +112,7 @@ class DCATDistribution(RDFModel):
             "rdf_type": "uri"
         }
     )
-    access_service: List[Union[AnyHttpUrl, DCATDataService]] = Field(
+    access_service: List[Union[AnyHttpUrl]] = Field(
         default=None,
         description="A data service that gives access to the distribution of the dataset",
         json_schema_extra={
@@ -134,7 +134,7 @@ class DCATDistribution(RDFModel):
         description="The size of a distribution in bytes.",
         json_schema_extra={
             "rdf_term": DCAT.byteSize,
-            "rdf_type": "xsd:nonNegativeInteger."
+            "rdf_type": "xsd:nonNegativeInteger"
         }
     )
     spatial_resolution: List[Union[float, LiteralField]] = Field(
